0205ca1abfcd77a2001c28e2300f71939661a167
popi753vol2 <luka.popiashvili09@gmail.com>
2026-01-26 20:32:51 +0400

    fix: add ref to dialog element in BurgerMenuModal for proper functionality

--- src/components/header/BurgerMenuModal.tsx
+++ src/components/header/BurgerMenuModal.tsx
@@ -23,7 +23,7 @@ const BurgeMenuModal = forwardRef<HTMLDialogElement>((_props, ref) => {
     const navigate = useNavigate();
 
     return (
-        <dialog onClick={handleBackdropClick} className="box-content w-full h-fit px-6 bg-white border-2 border-gray-300 sm:hidden backdrop:backdrop-blur-sm">
+        <dialog ref={dialog} onClick={handleBackdropClick} className="box-content w-full h-fit px-6 bg-white border-2 border-gray-300 sm:hidden backdrop:backdrop-blur-sm">
             <div className="w-full h-auto flex flex-col justify-start items-start gap-10 p-4 ">
                 <div className="w-full flex flex-row justify-between">
                     <Link to="/">
