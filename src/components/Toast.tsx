d132e5938a1a1b9a6ee733a33d48a7230c3bfe3e
popi753vol2 <luka.popiashvili09@gmail.com>
2026-02-02 15:38:47 +0400

    feat: refactor Toast and ToastContext for improved readability and consistency

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import clsx from "clsx";
-import { type Toast } from "@/contexts/ToastContext";
+import { cn } from "@/helpers/utils";
+import { type Toast } from "@/contexts";
+
+const borderDissapearingTime = 3000; 
 
 export default function Toast({ type, toast, icon }: { type: string, toast: Toast, icon: React.ReactNode }) {
   const [dissaperBorder, setDissaperBorder] = useState(false);
@@ -8,14 +10,13 @@ export default function Toast({ type, toast, icon }: { type: string, toast: Toas
   useEffect(() => {
     const timeout = setTimeout(() => {
       setDissaperBorder(true)
-    }, 3000);
+    }, borderDissapearingTime);
     return () => clearTimeout(timeout);
   }, []);
 
-
   return (
     <div className="absolute z-100 top-5 right-5 flex gap-2.5 w-86 max-h-40 text-white ">
-      <div className={clsx("w-full h-full rounded-lg px-4 py-3 gap-4 bg-gray-700 shadow-[0px_16px_24px_0px_#00000024] flex flex-row items-center p-4 border-b-4 duration-3000!",
+      <div className={cn("w-full h-full rounded-lg px-4 py-3 gap-4 bg-gray-700 shadow-[0px_16px_24px_0px_#00000024] flex flex-row items-center p-4 border-b-4 duration-3000!",
         {
           "border-green-500": type === "success" && !dissaperBorder,
           "border-yellow-500": type === "warning" && !dissaperBorder,
@@ -23,7 +24,7 @@ export default function Toast({ type, toast, icon }: { type: string, toast: Toas
           "border-transparent": dissaperBorder
         })}
       >
-        <div className={clsx("bg-gray-600 min-w-8 h-8 flex items-center justify-center rounded-full",
+        <div className={cn("bg-gray-600 min-w-8 h-8 flex items-center justify-center rounded-full",
           {
             "shadow-[5px_0_75px_20px_green]": type === "success",
             "shadow-[5px_0_75px_20px_yellow]": type === "warning",
--- src/contexts/ToastContext.tsx
+++ src/contexts/ToastContext.tsx
@@ -8,6 +8,8 @@ export type Toast = {
 
 export const ToastContext = createContext<(type: string, toast: Toast) => void>(() => { })
 
+const toastDissapearingTime = 5000;
+
 export const ToastProvider = ({ children }: { children: React.ReactNode }) => {
     const [type, setType] = useState("")
     const [toast, setToast] = useState<Toast>({ header: "", message: "" })
@@ -18,18 +20,17 @@ export const ToastProvider = ({ children }: { children: React.ReactNode }) => {
 
         setTimeout(() => {
             setType("");
-        }, 5000)
+        }, toastDissapearingTime)
     }, []);
 
     return (
         <ToastContext.Provider value={showToast}>
             {children}
-            {type === "success" ? <Toast key={Date.now()} type={type} toast={toast} icon={<CheckCircle />} />
-                : type === "error" ? <Toast key={Date.now()} type={type} toast={toast} icon={<ErrorCircle />} />
-                    : type === "warning" ? <Toast key={Date.now()} type={type} toast={toast} icon={<WarningCircle />} />
-                        : null}
+            {type === "success" && <Toast key={Date.now()} type={type} toast={toast} icon={<CheckCircle />} />}
+            {type === "error" && <Toast key={Date.now()} type={type} toast={toast} icon={<ErrorCircle />} />}
+            {type === "warning" && <Toast key={Date.now()} type={type} toast={toast} icon={<WarningCircle />} />}
         </ToastContext.Provider>
     )
 };
 
-export const useToast = () => useContext(ToastContext)
+export const useToast = () => useContext(ToastContext);
