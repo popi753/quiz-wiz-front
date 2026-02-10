f08bc6d824e44809a8a134f30d9957e630f52e8a
popi753vol2 <luka.popiashvili09@gmail.com>
2026-02-10 23:00:45 +0400

    feat: add env.example file

--- src/services/sanctumCsrfCookie.ts
+++ src/services/sanctumCsrfCookie.ts
@@ -1,6 +1,5 @@
 import apiAxiosInstance from "./apiAxiosInstance";
 
 export default function sanctumCsrfCookie() {
-    return true;
     apiAxiosInstance.get(import.meta.env.VITE_BASE_URL + '/sanctum/csrf-cookie');
 };
