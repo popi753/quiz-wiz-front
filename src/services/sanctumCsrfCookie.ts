c1ba0e11c40be07cbe78be81fe240ffbd03d2a05
popi753vol2 <luka.popiashvili09@gmail.com>
2026-02-11 21:30:19 +0400

    refactor: change API URL configuration

--- src/services/sanctumCsrfCookie.ts
+++ src/services/sanctumCsrfCookie.ts
@@ -1,5 +1,5 @@
 import apiAxiosInstance from "./apiAxiosInstance";
 
 export default function sanctumCsrfCookie() {
-    apiAxiosInstance.get(import.meta.env.VITE_BASE_URL + '/sanctum/csrf-cookie');
+    apiAxiosInstance.get(import.meta.env.VITE_API_URL + '/sanctum/csrf-cookie');
 };
