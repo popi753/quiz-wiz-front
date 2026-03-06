906d63120ea63c82f239cd27dba3c2e60708ff61
popi753vol2 <luka.popiashvili09@gmail.com>
2026-03-06 16:50:00 +0400

    fix: fix formatting according to the review

--- src/routes/QuizFillingPage.tsx
+++ src/routes/QuizFillingPage.tsx
@@ -12,7 +12,6 @@ export default function QuizFillingPage() {
         retry: false,
     });
 
-
     if (isLoading) {
         return <div className="loader">Loading...</div>;
     }
@@ -21,12 +20,9 @@ export default function QuizFillingPage() {
         return <ErrorPage errorCode={404} errorText="oops" icon={<Error404Icon />} />;
     }
 
-
     return (
         <div className="flex-1 w-full h-full flex flex-col items-center gap-24 px-22 py-15">
 
-
         </div>
-
     );
-}
+};
