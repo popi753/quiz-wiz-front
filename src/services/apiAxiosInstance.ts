3dc66d11df40ea86b8c4c16be2b94a4d8fa91f80
popi753 <luka.popi.919@gmail.com>
2026-03-14 11:28:27 +0400

    fix: refactor App component to fetch csrf token with useApp hook

--- src/services/apiAxiosInstance.ts
+++ src/services/apiAxiosInstance.ts
@@ -2,9 +2,11 @@ import axios from 'axios';
 const apiAxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL + '/api',
   withCredentials: true,
+  withXSRFToken: true,
   headers: {
     "Accept": 'application/json',
     "Content-Type": 'application/json',
+    'X-Requested-With': 'XMLHttpRequest',
   },
 });
 
