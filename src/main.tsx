3784b9b282579976c8d9c4cf18b9b5557bfc1d34
popi753 <luka.popi.919@gmail.com>
2026-03-14 11:45:43 +0400

    fix: remove StrictMode and ReactQueryDevtools for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
 
 import App from "@/App.tsx";
 import { ToastProvider, UserContextProvider } from "./contexts";
@@ -12,7 +10,6 @@ import "@/index.css";
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <UserContextProvider>
         <ToastProvider>
@@ -21,7 +18,5 @@ createRoot(document.getElementById("root")!).render(
           </BrowserRouter>
         </ToastProvider>
       </UserContextProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </StrictMode>
 );
